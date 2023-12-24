0ada94008f1ea226bcb3a28af6b9c6aa81880f25
AmeliaMatthiesen <amelia@matthiesen.se>
2023-12-24 20:56:28 +0100

    sista minuten uppdatering

--- main.js
+++ main.js
@@ -17,25 +17,56 @@ function renderPost(post) {
   resultsContainer.append(tags);
 
   let reactions = document.createElement('p');
-  reactions.textContent = 'Votes: ' + post.reactions;
+  reactions.textContent = 'Likes: ' + post.reactions;  // Display the number of likes
   reactions.classList.add('votes-class');
   resultsContainer.append(reactions);
+
+  // Like button
+  let likeButton = document.createElement('button');
+  likeButton.textContent = 'Like';
+  likeButton.addEventListener('click', () => {
+    // Increment the reactions count and update the UI
+    post.reactions++;
+    reactions.textContent = 'Likes: ' + post.reactions;
+  });
+  resultsContainer.append(likeButton);
 }
+
 
 const form = document.querySelector('form');
 const url = 'https://dummyjson.com/posts/';
+
+//Create a function to save a post in local storage
+function savePost(post) {
+  //Generate a unique key for the post
+  let key = 'post-' + Date.now();
+  //Convert the post object to a string
+  let value = JSON.stringify(post);
+  //Save the post in local storage with the key and value
+  localStorage.setItem(key, value);
+}
 
 //Create a function to fetch and display the posts
 function getPosts() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      //Clear the previous posts
+      let localPosts = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if (key.startsWith('post-')) {
+          let value = localStorage.getItem(key);
+          let post = JSON.parse(value);
+          localPosts.push(post);
+        }
+      }
+
       document.querySelector('#post-main').innerHTML = '';
-      //Render the new posts
-      data.posts.forEach((post) => renderPost(post));
+      let allPosts = data.posts.concat(localPosts);
+      allPosts.forEach((post) => renderPost(post));
     })
 }
+
 
 //Call the function when the page loads
 getPosts();
@@ -44,25 +75,16 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const formData = new FormData(form);
-  //Convert the FormData to a plain object
   const object = Object.fromEntries(formData.entries());
-  //Stringify the object to JSON
   const json = JSON.stringify(object);
 
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: json
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      //Close the modal window
-      modal.style.display = "none";
-      //Fetch and display the posts again
-      getPosts();
-    })
+  //Save the post in local storage
+  savePost(object);
+
+  //Close the modal window
+  modal.style.display = "none";
+  //Update the posts on the page
+  getPosts();
 })
 
 // Open an window for creating a post
@@ -73,15 +95,11 @@ let span = document.getElementsByClassName("close")[0];
 btn.onclick = function () {
   modal.style.display = "block";
 }
-
 span.onclick = function () {
   modal.style.display = "none";
 }
-
-
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-
 }
